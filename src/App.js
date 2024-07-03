79e950d6cd0a512aa10d294831efe58c73eecada
Aryan <69583042+aryannxroot@users.noreply.github.com>
2024-07-03 18:16:14 +0530

    Added files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 
+
 function App() {
+  
+  const [time,setTime] = useState(0);
+  const [running,setRunning] = useState(false);
+  let interval = useRef();
+
+  useEffect(() => {
+
+    if(running){
+      interval.current = setInterval(() => {
+        setTime(prevTime => prevTime + 10)
+      },10);
+    }else if(!running){
+      clearInterval(interval.current);
+    }
+
+    return () => {
+      clearInterval(interval.current);
+    }
+
+  },[running]);
+  
+  const handleReset = () => {
+    setRunning(false);
+    clearInterval(interval);
+    setTime(0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='w-full h-screen flex flex-col items-center justify-center py-8'>
+    <h1 className='text-2xl font-semibold pb-2 '>01-Stopwatch</h1>
+    <div className='flex space-x-1 font-semibold py-4'>
+      <span>{('0' + Math.floor((time / 60000) % 60)).slice(-2)} :</span>
+      <span>{('0' + Math.floor((time / 1000) % 60)).slice(-2)} :</span>
+      <span>{('0' + Math.floor((time / 10) % 100)).slice(-2)}</span>
+    </div>
+     
+    <div className='w-1/5 flex flex-row justify-between'>
+      {
+        running 
+        ?   <button className='border rounded-lg  py-2 px-10 ' 
+              onClick={() => setRunning(false)}>
+              Stop
+            </button>
+        :   <button className='border rounded-lg  py-2 px-10 '
+              onClick={() => setRunning(true)}>
+              Start
+              </button>
+      }
+      <button className='border rounded-lg  py-2 px-10 '
+        onClick={() => handleReset()}>
+        Reset
+      </button>
+    </div>
     </div>
   );
 }
